966d74c160b431c922abf0d88e0d73dd50eb05fb
abderrahman-bouhmad <123723691+abderrahman-bouhmad@users.noreply.github.com>
2025-04-26 12:05:14 +0100

    add login route

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, session
 from cs50 import SQL
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
@@ -31,6 +32,35 @@ def register():
         hash_pw = generate_password_hash(password)
         db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", username, hash_pw)
 
+
         return redirect("/login")
 
     return render_template("register.html")
+
+# login
+@app.route("/login", methods=["GET, POST"])
+def login():
+    # clear existing session, ensuring no previous user is logged in.
+    session.clear()
+
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        # Check for missing fields
+        if not username or not password:
+            flash("Please enter both fields.")
+            return redirect("/login")
+
+        # Check if the username exists
+        row = db.execute("SELECT * FROM users WHERE username = ?", username)
+        if len(row) != 1 or not check_password_hash(row[0]["hash"], password):
+            flash("Invalid username or password.")
+            return redirect("/login")
+
+        # If the login is successful, store the user’s ID in the session and redirect them to the task list page
+        session["user_id"] = row[0]["if"]
+        return redirect("/")
+
+    # Show the login form if the request method is GET
+    return render_template("login.html")
